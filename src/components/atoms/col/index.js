8a2c99b69ae3890353149a086cb653378b255f97
paulo101977 <paulo10.1977@yahoo.com.br>
2019-09-15 10:59:27 -0300

    Fix col and add gitlab-ci

--- src/components/atoms/col/index.js
+++ src/components/atoms/col/index.js
@@ -2,13 +2,13 @@ import styled from  'styled-components';
 
 const Col = styled.div.attrs({className: 'col'})``;
 
-const Col12 = styled(Col).attrs({className: 'col-md-12'})``;
+const Col12 = styled.div.attrs({className: 'col-md-12'})``;
 
-const Col6 = styled(Col).attrs({className: 'col-md-6'})``;
+const Col6 = styled.div.attrs({className: 'col-md-6'})``;
 
-const Col4 = styled(Col).attrs({className: 'col-md-4'})``;
+const Col4 = styled.div.attrs({className: 'col-md-4'})``;
 
-const Col3 = styled(Col).attrs({className: 'col-md-3'})``;
+const Col3 = styled.div.attrs({className: 'col-md-3'})``;
 
 export default Col;
 export { Col, Col12, Col6, Col4, Col3 };
